6cfb52ec46b15f736e0ed630163ff0e5f1b4c43b
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2024-04-19 21:30:39 -0300

    layout home svg + background

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,14 @@
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import {useNavigation } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import { DatabaseConnection } from '../../../databases/database';
 import { SvgUri } from 'react-native-svg';
-import Wavesvg from "../../../assets/wave.svg" ;
+import Wavesvg from "../../../assets/wave.svg";
 
 const db = new DatabaseConnection.getConnection;
 
- 
+
 export default function Home() {
   const navigation = useNavigation();
 
@@ -27,48 +27,48 @@ export default function Home() {
       tx.executeSql('CREATE TABLE IF NOT EXISTS telefones_has_clientes (id integer PRIMARY KEY AUTOINCREMENT, telefone_id INTEGER, cliente_id INTEGER, FOREIGN KEY (telefone_id) REFERENCES tbl_telefones(id), FOREIGN KEY (cliente_id) REFERENCES tbl_clientes(id) )')
       console.log('criado tbl telefoneshasclientes')
     })
-  },[])
+  }, [])
 
-  
+
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={{display:'flex',flexDirection:"column",alignItems:'center', gap:40}}>
-        <Text style={{fontSize:20}}>Home</Text>
-        <Text style={{fontSize:30,textAlign:'center',fontWeight:'900',color:'purple' }}>Seja bem vindo a sua lista telefonica</Text>
-        <TouchableOpacity onPress={() => {navigation.navigate("telefones")}} style ={{backgroundColor:'blue',width:"50%", borderRadius:5, padding:10}}>
-          <Text style={{color:'white',fontSize:20,textAlign:'center'}}>Ver contatos</Text>
+      <View style={{backgroundColor:'#aef' ,display: 'flex', flexDirection: "column", alignItems: 'center', justifyContent: "space-between", height: '100%' }}>
+        <Text style={{ fontSize: 20 }}>Home</Text>
+        <Text style={{ fontSize: 30, textAlign: 'center', fontWeight: '900', color: '#aef', textDecorationLine:'underline',width:'90%' ,borderRadius:20,padding:10, backgroundColor:'dodgerblue' }}>Seja bem vindo a sua lista telefonica</Text>
+        <TouchableOpacity onPress={() => { navigation.navigate("telefones") }} style={{ marginTop: 20, backgroundColor: 'blue', width: "50%", borderRadius: 5, padding: 10 }}>
+          <Text style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>Ver contatos</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => {navigation.navigate("registrarCliente")}} style ={{backgroundColor:'blue',width:"50%", borderRadius:5, padding:10}}>
-          <Text style={{color:'white',fontSize:20,textAlign:'center'}}>registrar contatos</Text>
+        <TouchableOpacity onPress={() => { navigation.navigate("registrarCliente") }} style={{ backgroundColor: 'blue', width: "50%", borderRadius: 5, padding: 10 }}>
+          <Text style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>registrar contatos</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => {navigation.navigate("editarRegistro")}} style ={{backgroundColor:'blue',width:"50%", borderRadius:5, padding:10}}>
-          <Text style={{color:'white',fontSize:20,textAlign:'center'}}>editar contato</Text>
+        <TouchableOpacity onPress={() => { navigation.navigate("editarRegistro") }} style={{ backgroundColor: 'blue', width: "50%", borderRadius: 5, padding: 10 }}>
+          <Text style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>editar contato</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => {navigation.navigate("deletaCliente")}} style ={{backgroundColor:'blue',width:"50%", borderRadius:5, padding:10}}>
-          <Text style={{color:'white',fontSize:20,textAlign:'center'}}>deletar contato</Text>
+        <TouchableOpacity onPress={() => { navigation.navigate("deletaCliente") }} style={{ backgroundColor: 'blue', width: "50%", borderRadius: 5, padding: 10 }}>
+          <Text style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>deletar contato</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => {navigation.navigate("pesquisarClientes")}} style ={{backgroundColor:'blue',width:"50%", borderRadius:5, padding:10}}>
-          <Text style={{color:'white',fontSize:20,textAlign:'center'}}>pesquisar contato</Text>
+        <TouchableOpacity onPress={() => { navigation.navigate("pesquisarClientes") }} style={{ backgroundColor: 'blue', width: "50%", borderRadius: 5, padding: 10 }}>
+          <Text style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>pesquisar contato</Text>
         </TouchableOpacity>
 
-        <Wavesvg
-          width="200%"
-          height={200}
-        />
+        <View style={{width:'100%',backgroundColor:'lightblue'}}>
+          <Wavesvg
+            style={{ borderWidth: 1, borderColor: "black" }}
+            width="200%"
+            height={200}
+          />
+        </View>
+
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
   safeArea: {
-    marginTop: 30
+    marginTop: 30,
+    height: '100%',
   }
 });
